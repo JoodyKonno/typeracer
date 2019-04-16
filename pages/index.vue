77fdcd90799caa4d6eacd800117776d27933d375
Joody Konno <joodykonno@hotmail.com>
2019-04-15 23:03:29 -0300

    Added the isFinished state
    Added an Watcher on inputedQuote model, for when the remainingText is equal empty string, it sets the isFinished to true.
    
    When the state is finished it displays a message and blocks the textarea

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section>
     <app-header></app-header>
 
-    <section class="hero is-primary">
+    <section class="hero is-info">
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
@@ -16,6 +16,11 @@
     </section>
 
     <div class="container">
+      <div class="tile is-child notification is-primary" v-show="isFinished">
+        <p class="title">Game Over</p>
+        <p class="subtitle">Your score: </p>
+      </div>
+
       <div class="card">
         <div class="card-content">
           <p class="title">
@@ -28,7 +33,7 @@
       <div class="card form-card">
         <div class="card-content">
           <p class="title">
-            <textarea v-model='inputedQuote'></textarea>
+            <textarea :disabled="isFinished" v-model='inputedQuote'></textarea>
           </p>
           <p class="subtitle">Type the text and see your score</p>
         </div>
@@ -45,6 +50,7 @@ export default {
     return {
       inputedQuote: '',
       selectedQuote: 'They had taken it all away from him now, they had turned away from him and there was nothing for him now. He was alone and there was nothing for him.',
+      isFinished: false,
     }
   },
 
@@ -77,6 +83,14 @@ export default {
       return this.selectedQuoteLetters
         .slice(this.matchedText.length)
         .join('');
+    },
+  },
+
+  watch: {
+    inputedQuote: function (newInput, oldInput) {
+      if (this.remainingText === '') {
+        this.isFinished = true;
+      }
     }
   },
 
